fdebb6c53acd4d04d73a00fb44b7a73da7d2c85a
Lauri Hartikka <lhartikk@gmail.com>
2014-03-11 16:36:18 +0200

    Use struct for endiannes handling

--- genesis.py
+++ genesis.py
@@ -2,6 +2,7 @@ import hashlib
 import binascii
 import struct
 import array
+import struct
 
 from construct import *
 
@@ -11,16 +12,19 @@ scriptSig = '04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616
 scriptPubKeyLen = '41'
 outputScriptPubKey = '04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f'
 outputScript = (scriptPubKeyLen + outputScriptPubKey + OP_CHECKSIG).decode('hex')
-startNonce = 0x1DAC2B7C
 
-version = '01000000'.decode('hex')
+startNonce = 2083236893
+time = 1231006505
+bits = 0x1d00ffff
+
+version = 1
 numInputs = 1
 numOutputs = 1
 locktime = 0
-prevOutput =  '\x00' * 32
+prevOutput =  struct.pack('<qqqq', 0,0,0,0)
 prevoutIndex = 0xFFFFFFFF
 sequence = 0xFFFFFFFF
-outValue = '00f2052a01000000'.decode('hex')
+outValue = 0x000000012a05f200 
 scriptSigLen = 0x4D
 outputScriptLen = 0x43
 
@@ -39,7 +43,7 @@ Bytes('outputScript',  outputScriptLen),
 UBInt32('locktime'))
 
 tx = transaction.parse('\x00'*204)
-tx.version = version
+tx.version = struct.pack('<I', version)
 tx.numInputs = numInputs
 tx.prevOutput = prevOutput
 tx.prevoutIndex = prevoutIndex
@@ -47,31 +51,18 @@ tx.scriptSigLen = scriptSigLen
 tx.scriptSig = scriptSig
 tx.sequence = sequence
 tx.numOutputs = numOutputs
-tx.outValue = outValue
+tx.outValue = struct.pack('<q' ,outValue)
 tx.outputScriptLen = outputScriptLen
 tx.outputScript = outputScript
 tx.locktime = locktime
 
-print "tx"
-print transaction.build(tx).encode('hex_codec')
-print "txover"
-
 hash = hashlib.sha256(hashlib.sha256(transaction.build(tx)).digest()).digest()
 hashMerkleRoot =hash.encode('hex_codec')
-hashPrevBlock = '\x00' * 32
-time = '29AB5F49'.decode('hex')
-bits = 'FFFF001D'.decode('hex')
-nonce = hex(startNonce)[2:].decode('hex')
-
-
-
-#chunks, chunk_size = len(hashMerkleRoot), 2
-#littleEndian = ''.join([ hashMerkleRoot[i:i+chunk_size] for i in range(0, chunks, chunk_size) ][::-1])
-
+hashPrevBlock = struct.pack('<qqqq', 0,0,0,0)
 
 print "merkle hash: " + hashMerkleRoot
-print "time: " + str(int(time.encode('hex_codec'), 16))
-print "bits: " + bits.encode('hex_codec')
+print "time: " + str(time)
+print "bits: " + str(bits)
 
 
 
@@ -85,27 +76,23 @@ blockHeader = Struct("blockHeader",
 
 
 genesisblock = blockHeader.parse('\x00'*80)
-genesisblock.version = version
+genesisblock.version = struct.pack('<I', version)
 genesisblock.hashPrevBlock = hashPrevBlock
 genesisblock.hashMerkleRoot = hashMerkleRoot.decode('hex')
-genesisblock.Time = time
-genesisblock.Bits = bits
-genesisblock.Nonce = nonce
+genesisblock.Time = struct.pack('<I', time)
+genesisblock.Bits = struct.pack('<I', bits)
+genesisblock.Nonce = struct.pack('<I', startNonce)
 
-print "block:"
-print blockHeader.build(genesisblock).encode('hex_codec')
-
-i = 0
+nonce = startNonce
 
 while True:
   genesisHash = hashlib.sha256(hashlib.sha256(blockHeader.build(genesisblock)).digest()).digest()
   blockHash = blockHeader.build(genesisblock)
 
-  if True or int(genesisHash.encode('hex_codec')[60:64], 16) == 0:
+  if int(genesisHash.encode('hex_codec')[60:64], 16) == 0:
     print 'genesis hash found!'
     print 'genesis hash: '+ genesisHash.encode('hex_codec')
     break
   else:
-    i = i + 1
-    nonce = nonce = hex(startNonce + i)[2:].decode('hex')
-    genesisblock.Nonce = nonce
+    nonce = nonce + 1
+    genesisblock.Nonce = struct.pack('<I', nonce)
